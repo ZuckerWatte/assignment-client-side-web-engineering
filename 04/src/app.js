69463941c5ebba48f90113f4f5e224d72d4474b7
Mona Fenzl <oranges_gummibearle@web.de>
2016-11-23 10:47:18 +0100

    setup node server and socket io

--- 04/src/app.js
+++ 04/src/app.js
@@ -1,2 +1,27 @@
-import config from './config'
+//import config from './config'
+
+var express = require('express');
+var app = express();
+app.use(express.static('./'));
+var http = require('http').Server(app);
+var port = 3000;
+var io = require('socket.io')(http);
+
+http.listen(port, () => {
+    console.log(`listening on *: ${port}`);
+});
+
+io.on('connection', (socket) => {
+  console.log('a chessplayer connected');
+  
+  socket.on('move', (data) => {
+    io.emit('move', data);
+    console.log(`move: ${data.from}-${data.to}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('a chessplayer disconnected');
+  });
+});
+
 
